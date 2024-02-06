e6d97c9b1de64554bf185ba0e5ed41056aacecfe
MassimoCastell <massimo.castell@gmail.com>
2024-02-06 19:56:43 +0100

    init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+            -- Highlight, edit, and navigate code
+            'nvim-treesitter/nvim-treesitter',
+            dependencies = {
+                'nvim-treesitter/nvim-treesitter-textobjects',
+            },
+            build = ':TSUpdate'
+            config = function()
+                local config = require("nvim-treesitter.configs")
+                config.setup({
+                    ensure_installed = {"lua", "python", "go"},
+                    highlight = {enable = true },
+                    indent = { enable = true },
+                })
+                end
+                
+        }

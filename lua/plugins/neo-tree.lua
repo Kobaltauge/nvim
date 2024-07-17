9ab03f7af42daff6d4d965948366ebc356b68300
debiecas <Massimo.Castell@durr.com>
2024-07-17 12:27:01 +0200

    works now on windows

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,19 +3,11 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim", -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
   },
   config = function()
-    require("neo-tree").setup({
-      window = {
-        mappings = {
-          ["h"] = "open",
-          ["l"] = "open",
-        },
-      },
-    })
-    -- vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
   end,
 }

0b22710686bd0cf1adf8f05be8118ccf7edddf5f
Massimo Castell <Massimo.Castell@durr.com>
2025-02-16 16:18:54 +0100

    lsp errors

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,44 +1,42 @@
 return {
   {
     "williamboman/mason.nvim",
-    lazy = false,
+    -- lazy = false,
     config = function()
       require("mason").setup({
-        PATH = "prepend" -- "skip" seems to cause the spawning error
+        PATH = "prepend", -- "skip" seems to cause the spawning error
       })
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = false,
+    -- lazy = false,
+    config = function()
+      require("mason-lspconfig").setup()
+    end,
     opts = {
       auto_install = true,
     },
   },
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
+    -- lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
       lspconfig.html.setup({
         capabilities = capabilities
       })
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-      lspconfig.jedi_language_server.setup({
+      lspconfig.harper_ls.setup({
         capabilities = capabilities
       })
       lspconfig.powershell_es.setup({
         bundle_path = 'C:/Users/debiecas/AppData/Local/nvim-data/mason/packages/powershell-editor-services',
         capabilities = capabilities
       })
-
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})

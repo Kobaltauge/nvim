74be2063639b80659039bd935e1bcff8029c6775
Massimo Castell <Massimo.Castell@durr.com>
2025-03-06 23:25:14 +0100

    rework with kickstart as template

--- init.lua
+++ init.lua
@@ -1,111 +1,12 @@
+if (vim.g.nvy) then
+  vim.o.guifont="Cousine Nerd Font:h10"
+  vim.cmd('cd %:p:h')
+end
+
 if (vim.g.vscode) then
   -- VSCode extension
   require("vim-options-vsc")
 else
   require("vim-options")
-  vim.loader.enable()
-
-  if (vim.g.nvy) then
-    vim.o.guifont="Cousine Nerd Font:h10"
-    vim.cmd('cd %:p:h')
-  end
-  -- [[ Install `lazy.nvim` plugin manager ]]
-  --    https://github.com/folke/lazy.nvim
-  --    `:help lazy.nvim.txt` for more info
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-      lazypath,
-    })
-  end
-  vim.opt.rtp:prepend(lazypath)
-  require("lazy").setup("plugins")
-
-  -- checkhealth fixes
-  vim.g.loaded_python3_provider = 0
-  vim.g.loaded_perl_provider = 0
-
-  -- Set highlight on search
-  vim.o.hlsearch = false
-
-  -- Make relative line numbers default
-  vim.wo.number = true
-  vim.wo.relativenumber = true
-
-  -- Enable mouse mode
-  vim.o.mouse = 'a'
-
-  -- Sync clipboard between OS and Neovim.
-  --  Remove this option if you want your OS clipboard to remain independent.
-  --  See `:help 'clipboard'`
-  -- vim.o.clipboard = 'unnamedplus'
-
-  -- Enable break indent
-  vim.o.breakindent = true
-
-  -- Save undo history
-  vim.o.undofile = true
-
-  -- Case-insensitive searching UNLESS \C or capital in search
-  vim.o.ignorecase = true
-  vim.o.smartcase = true
-
-  -- Keep signcolumn on by default
-  vim.wo.signcolumn = 'yes'
-
-  -- Decrease update time
-  vim.o.updatetime = 250
-  vim.o.timeoutlen = 300
-
-  -- Set completeopt to have a better completion experience
-  vim.o.completeopt = 'menuone,noselect'
-
-  -- NOTE: You should make sure your terminal supports this
-  vim.o.termguicolors = true
-
-  -- [[ Basic Keymaps ]]
-
-  -- Keymaps for better default experience
-  -- See `:help vim.keymap.set()`
-  vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
-  -- word wrap
-  vim.keymap.set('n', '<leader>w', ":set wrap!<CR>", { silent = true })
-
-  -- Remap for dealing with word wrap
-  vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-  vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
-  -- Diagnostic keymaps
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
-  -- copy path and file into clipboard
-  vim.keymap.set('n', '<leader>cp', "n:let @+=expand('%:p')<CR>", { silent = true })
-
-  -- [[ Highlight on yank ]]
-  -- See `:help vim.highlight.on_yank()`
-  local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-  vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
-  })
-
-  -- rainbow csv filetype
-  vim.cmd([[
-    autocmd FileType csv setlocal expandtab
-    autocmd BufNewFile,BufRead *.csv set filetype=csv_semicolon
-  ]])
-
 end
 
--- lua/vim-options-vsc.lua
+++ lua/vim-options-vsc.lua
@@ -1,9 +1,9 @@
-vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set expandtab")
+vim.cmd("set softtabstop=2")
 vim.g.mapleader = " "
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = " "
 
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', { silent = true })

a3bcb5020337b73edafb0ffa73816eb9b154095b
Massimo Castell <Massimo.Castell@durr.com>
2025-04-06 11:16:27 +0200

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
 return {
-{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-},
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			ensure_installed = { "lua", "python", "go", "yaml", "markdown_inline" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }

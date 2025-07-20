cfeb3d2a4ced058d14f16a4448b6f5b6b7096332
Massimo <massimo@kobaltauge.de>
2025-07-20 17:20:32 +0200

    active line

--- lua/config/cas_config.lua
+++ lua/config/cas_config.lua
@@ -3,6 +3,10 @@ if vim.g.nvy then
   vim.cmd("cd %:p:h")
 end
 
+if vim.fn.has("unix") == 1 and string.match(vim.fn.system("uname"), "Linux") then
+  vim.o.shell = "/bin/bash"
+end
+
 if (vim.g.vscode) then
   -- VSCode extension
   require("config.vim-options-vsc")

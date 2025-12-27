db6b5605df042e0a8e5ab5e9da866274cd70bd57
Massimo Castell <Massimo.Castell@durr.com>
2025-12-27 20:44:54 +0100

    neovide config

--- lua/config/cas_config.lua
+++ lua/config/cas_config.lua
@@ -3,6 +3,23 @@ if vim.g.nvy then
   vim.cmd("cd %:p:h")
 end
 
+if vim.g.neovide then
+  vim.o.guifont = "Cousine Nerd Font:h10"
+  vim.cmd("cd %:p:h")
+  -- vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  -- vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  -- vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  -- vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  -- vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  -- vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
 if vim.fn.has("unix") == 1 and string.match(vim.fn.system("uname"), "Linux") then
   vim.o.shell = "/bin/bash"
 end
